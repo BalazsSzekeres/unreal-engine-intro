1a1cc959d82a9f4909536fa6202a4e2f258d73a5
Balazs Szekeres <balazs@bspace.nl>
2021-07-01 00:18:48 +0200

    Added basic terminal logic to cartridge

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,22 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Hello There!"));
+    PrintLine(TEXT("Press TAB to enable console"));
+    HiddenWorld = TEXT("General Kenobi");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ClearScreen();
+    
 
+    if (Input == HiddenWorld)
+    {
+        PrintLine(TEXT("This is where the fun begins!"));
+    }
+    else
+    {
+        PrintLine(TEXT("So uncivilized."));
+    }
 }
